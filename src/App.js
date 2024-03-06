25186d1cb9c8084b02af99ea0a61bebbd07afb34
hrishikeshtibile <hrishitibile@gmail.com>
2024-03-06 09:07:58 +0530

    Added navbar functionality component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Display } from './Components/Display.js';
 import { Login } from './Components/Login.js';
+import { Navbar } from './Components/Navbar.js';
 import { Signup } from './Components/Signup.js';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Signup />
       <Login />
       <Display />
