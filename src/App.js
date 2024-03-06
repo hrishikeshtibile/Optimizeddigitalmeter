a2e7554fb5a3178a229e09f150ee61f35059d9a4
hrishikeshtibile <hrishitibile@gmail.com>
2024-03-06 09:01:42 +0530

    Added signup and login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { Display } from './Components/Display.js';
+import { Login } from './Components/Login.js';
+import { Signup } from './Components/Signup.js';
 
 function App() {
   return (
     <div>
+      <Signup />
+      <Login />
       <Display />
     </div>
   );
